5e1d6ef2ac116e0293b9da690f3803ae80073f23
Admin <ganapathiselva16@gmail.com>
2019-08-01 17:24:55 +0530

    inital commit

---
+++ app/src/main/java/com/example/dboperations/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.dboperations
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.DatabaseErrorHandler
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.annotation.IntegerRes
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.example.utils.DatabaseHelper
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    var databaseHelper: DatabaseHelper? = null
+    var edtUserName: EditText? = null
+    var edtMark1: EditText? = null
+    var edtMark2: EditText? = null
+    var edtTot: EditText? = null
+    var total: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        databaseHelper = DatabaseHelper(this@MainActivity)
+        edtUserName = findViewById<View>(R.id.edtUserName) as EditText
+        edtMark1 = findViewById<View>(R.id.edtTotMark1) as EditText
+        edtMark2 = findViewById<View>(R.id.edtMark2) as EditText
+        edtTot = findViewById<View>(R.id.edtTot) as EditText
+    }
+
+
+    fun dbInsert(v: View): Unit {
+//        var flag: Boolean
+//        var r = StringBuilder("Hello12121 World")
+//        for (i in 0 until r.length) {
+//            if ('A' >= r[i] <= 'Z')
+//                r.setCharAt(i, '\u0000')
+//        }
+
+        total = 0
+        var a = Integer.parseInt(edtMark1?.text.toString())
+        var b = Integer.parseInt(edtMark2?.text.toString())
+        total = a + b
+        edtTot?.setText(total.toString())
+
+        if (databaseHelper?.dbInsert(edtUserName?.text.toString(), a, b, total)!!) {
+            Toast.makeText(this@MainActivity, "Insert Success", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this@MainActivity, "Insert Failed ", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun dbGetAllData(v: View) {
+//        databaseHelper?.dbSelectAll(edtUserName?.text.toString())
+        databaseHelper?.getAllData()
+    }
+
+
+//    fun dbDelete(v: View): Unit {
+//        val del: Int = databaseHelper?.dbDelete("2")!!
+//        if (del > 0)
+//            Toast.makeText(this@MainActivity, "Data Deleted", Toast.LENGTH_LONG).show()
+//        else
+//            Toast.makeText(this@MainActivity, "Data not Deleted", Toast.LENGTH_LONG).show()
+//    }
+//
+//
+//    fun dbUpdate(v: View): Unit {
+//
+//    }
+//
+//    fun dbView(v: View): Unit {
+//        var cur: Cursor = databaseHelper?.getAllTableData()!!
+//        try {
+//
+//            if (cur.moveToFirst()) {
+//
+//                while (!cur.isAfterLast()) {
+//
+//                    Toast.makeText(this@MainActivity, "Table Name=> " + cur.getString(0), Toast.LENGTH_LONG).show()
+//                    cur.moveToNext()
+//                }
+//
+//            }
+//
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
+//    }
+
+}
---
+++ app/src/main/java/com/example/utils/DatabaseHelper.kt
@@ -0,0 +1,124 @@
+package com.example.utils
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+import java.lang.StringBuilder
+
+class DatabaseHelper(var context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private val DATABASE_NAME: String = "STUDENT_DB"
+        private val TABLE_NAME: String = "STUDENT_TABLE"
+        private val UID: String = "id"
+        private val NAME: String = "Name"
+        private val MARK1: String = "Mark1"
+        private val MARK2: String = "Mark2"
+        private val TOTAL: String = "Total"
+        private val DATABASE_VERSION: Int = 1
+    }
+
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        try {
+            p0?.execSQL(
+                "CREATE TABLE $TABLE_NAME( $UID INTEGER PRIMARY KEY AUTOINCREMENT ,$NAME VARCHAR(255)," +
+                        "$MARK1 VARCHAR(25),$MARK2 VARCHAR(25),$TOTAL VARCHAR(25) )"
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        try {
+//            p0?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+//            onCreate(p0)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+    fun dbInsert(name: String, m1: Int, m2: Int, tot: Int): Boolean {
+
+        var db: SQLiteDatabase = this.writableDatabase
+        var cv: ContentValues = ContentValues()
+        cv.put(NAME, name)
+        cv.put(MARK1, m1)
+        cv.put(MARK2, m2)
+        cv.put(TOTAL, tot)
+        var id: Long = db.insert(TABLE_NAME, null, cv)
+
+
+        return if (id > -1) true else false
+
+//
+
+    }
+
+
+    fun getAllData(): Unit {
+
+        val db: SQLiteDatabase = this.writableDatabase
+        var columns = arrayOf<String>(DatabaseHelper.UID, DatabaseHelper.NAME)
+        var cursor: Cursor = db.query(TABLE_NAME, columns, null, null, null, null, null)
+        cursor.moveToFirst()
+        if (cursor.count != 0 && cursor.moveToFirst()) {
+
+            while (cursor.moveToNext()) {
+                val id: Int = cursor.getInt(cursor.getColumnIndex(UID))
+                val name = cursor.getString(cursor.getColumnIndex(NAME))
+//                var m1 = cursor.getString(cursor.getColumnIndex(MARK1))
+//                var m2 = cursor.getString(cursor.getColumnIndex(MARK2))
+//                var tot: String = cursor.getString(cursor.getColumnIndex(TOTAL))
+
+                Toast.makeText(context, " " + name + " ", Toast.LENGTH_SHORT).show()
+
+
+            }
+
+        } else {
+            Toast.makeText(context, "No data Found", Toast.LENGTH_SHORT).show()
+        }
+
+        db.close()
+
+    }
+
+
+    fun dbSelectAll(na: String): Unit {
+        val db: SQLiteDatabase = this.writableDatabase
+        var columns = arrayOf<String>(DatabaseHelper.UID, DatabaseHelper.NAME)
+        var cursor: Cursor =
+            db.query(TABLE_NAME, columns, DatabaseHelper.NAME + "='" + na + "'", null, null, null, null, null)
+
+        if (cursor.count != 0) {
+            while (cursor.moveToNext()) {
+//                val index = cursor.getColumnIndex(UID)
+                val id = cursor.getInt(cursor.getColumnIndex(UID))
+                val Name = cursor.getString(cursor.getColumnIndex(NAME))
+                Toast.makeText(
+                    context, " ID : " + id + " Name :  " + Name,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else {
+            Toast.makeText(context, " No data Found", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+
+
+    fun dbDelete(id: String): Int {
+        val db: SQLiteDatabase = this.writableDatabase
+        return db.delete(TABLE_NAME, "ID = ?", arrayOf<String>(id))
+    }
+
+
+}
